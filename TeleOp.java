c3ffd28c45940509a7ac1d868ade47a31aa9da6d
QuantumMango <tricad12@gmail.com>
2017-01-11 16:17:03 -0600

    Add files via upload
    Updated code as of 1/11/17
    More advanced autonomous pending

--- TeleOp.java
+++ TeleOp.java
@@ -14,28 +14,28 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="Drive", group="TeleOp")
 public class TeleOp extends LinearOpMode {
     @Override
-    public void runOpMode() throws InterruptedException{
+    public void runOpMode() throws InterruptedException {
         HardwareRobot robot = new HardwareRobot();
         robot.init(hardwareMap);
         robot.left.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         robot.right.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        robot.launcher.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        //robot.launcher.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         //robot.yoga.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        robot.bar.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        //robot.bar.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         waitForStart();
 
-        while(opModeIsActive()){
+        while (opModeIsActive()) {
             robot.left.setPower(gamepad1.left_stick_y);
             robot.right.setPower(gamepad1.right_stick_y);
             //foreward
-            if (gamepad1.left_trigger>.05)
+            /*if (gamepad1.left_trigger > .05)
                 robot.bar.setPower(-gamepad1.left_trigger);
-            //reverse
-            else if (gamepad1.right_trigger>.05)
+                //reverse
+            else if (gamepad1.right_trigger > .05)
                 robot.bar.setPower(gamepad1.right_trigger);
-            else
-                robot.bar.setPower(0);
+0000000000000000000000000000000000000000000            else
+                robot.bar.setPower(0);*/
 
             /*if (gamepad2.left_stick_y > 0.5){
                robot.yoga.setPower(0.5);
@@ -45,18 +45,19 @@ public class TeleOp extends LinearOpMode {
                robot.yoga.serPower(-gamepad2.left_stick_y);
             }
             */
-            if (gamepad1.x){
+            if (gamepad1.x) {
                 robot.left.setDirection(DcMotorSimple.Direction.REVERSE);
                 robot.right.setDirection(DcMotorSimple.Direction.REVERSE);
             }
-            if (gamepad1.y){
+            if (gamepad1.y) {
                 robot.left.setDirection(DcMotorSimple.Direction.FORWARD);
                 robot.right.setDirection(DcMotorSimple.Direction.FORWARD);
             }
-            if (gamepad1.a)
+            /*if (gamepad1.a)
                 robot.launcher.setPower(-1.0);
             if (gamepad1.b)
-                robot.launcher.setPower(0.0);
+                robot.launcher.setPower(0.0);*/
+        }
     }
 }
 
